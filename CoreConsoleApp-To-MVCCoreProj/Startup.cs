6557319511d641781bb5a18e3cba9469cde5672b
Nick Saravia <nick.saravia@gmail.com>
2020-06-13 13:18:03 -0400

    configure statup.cs to run normal mvc controllers with views. this is not going to use razor pages.

--- CoreConsoleApp-To-MVCCoreProj/Startup.cs
+++ CoreConsoleApp-To-MVCCoreProj/Startup.cs
@@ -1,11 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace CoreConsoleApp_To_MVCCoreProj
 {
     public class Startup
     {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvcCore();
+            services.AddControllersWithViews();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseRouting();
+            app.UseStaticFiles();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
     }
 }
