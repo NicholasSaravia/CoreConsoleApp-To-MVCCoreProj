ab6fb137fdc533334ed46a79ccd7492abe933f1f
Nick Saravia <nick.saravia@gmail.com>
2020-06-13 13:03:52 -0400

    Add project files.

---
+++ CoreConsoleApp-To-MVCCoreProj/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoreConsoleApp_To_MVCCoreProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
